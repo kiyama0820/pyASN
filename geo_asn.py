8bd78ead6569251243d1ef0263f8ac86f029c540
splus28 <64972690+splus28@users.noreply.github.com>
2023-07-04 13:13:16 +0900

    Update geo_asn.py

--- geo_asn.py
+++ geo_asn.py
@@ -1,11 +1,9 @@
 import geoip2.database
-import tqdm
 
 input,output=input().split()
 reader = geoip2.database.Reader('GeoLite2-ASN.mmdb')
 output_file = open(output, 'a')
 with open(input) as f:
-    #for i, l in tqdm.tqdm(enumerate(f)):
     for i, l in enumerate(f):
         ip = l[63:104][:l[63:104].find("#")]  
         target = str(reader.asn(ip).autonomous_system_number)+"\n"
