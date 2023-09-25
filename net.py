6724ab5b13fe98f55078ed67fb37adf66cf5f5fe
splus28 <64972690+splus28@users.noreply.github.com>
2023-09-25 11:03:12 +0900

    Update net.py

--- net.py
+++ net.py
@@ -1,19 +1,56 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-import numpy as np
 
-edgewidth = []
-G = nx.DiGraph()
-f = open('a22.tyo.20230710', 'r')
-for i in f:
-    i = i.replace('\', ','\',').split()
-    edgewidth.append(int(i.pop(0)))
-    path=i[1].replace('[\'','').replace('i\']','').split('\',\'')
-    path.pop(-1)
-    nx.add_path(G, path)
-vmin = np.array(edgewidth).min()
-vmax = np.array(edgewidth).max()
-b = (edgewidth - vmin).astype(float) / (vmax - vmin).astype(float)
-#nx.draw_networkx(G,width = (b+0.1)*10, alpha=0.5)
-nx.draw_networkx(G,alpha=0.5)
-plt.show()
+def main():
+  file_name='a22.tyo.20230710.ip'
+  G = nx.DiGraph()
+  counter,path=make_list(file_name)
+  max_co=counter[-1]
+  for i,pa in enumerate(path):
+    print(counter[i],pa)
+    nx.add_path(G, pa,color=e_color(max_co,counter[i]))
+
+  edge_color = [edge["color"] for edge in G.edges.values()]
+  nx.draw_networkx(G,alpha=0.5, edge_color=edge_color)
+  plt.savefig(file_name+".png") 
+  plt.show()
+
+def e_color(max_co,counter):
+   co_ratio=counter/max_co
+   c = int(((-co_ratio / 2) + 0.5) * 255)
+   if co_ratio >= 0.75:
+     return get_colocode(255-c, c, 0)
+   elif co_ratio >= 0.5:
+     return get_colocode(c, 255-c, 0)
+   elif co_ratio >= 0.25:
+     return get_colocode(0, 255-c, c)
+   else:
+     return get_colocode(0, c, 255-c)
+
+def get_colocode(r,g,b):
+    return '#%02X%02X%02X' % (r,g,b)
+
+def make_list(file):
+  f = open(file, 'r')
+  counter =  []
+  path=[]
+  for i in f:
+      i = i.replace('\', \'','\',\'').split()
+      p=[i[2].replace('[[\'','').replace('\']]','').split('\',\'')]
+      p=list(filter(lambda x: x!='i', p[0]))
+      p=list(filter(lambda x: x!='100', p))
+      p=list(filter(lambda x: x!='0', p))
+      p=list(dict.fromkeys(p)) # Duplicate Delete
+      p.insert(0, 0) # Initial value insertion
+      if p in path: # Combine duplicate routes
+        counter[path.index(p)]+=int(i[0])
+      else:
+        path.append(p)
+        counter.append(int(i[0]))
+  zip_lists = zip(counter, path)
+  zip_sort = sorted(zip_lists)
+  counter, path = zip(*zip_sort)
+  return counter,path
+
+if __name__ == "__main__":
+    main()
